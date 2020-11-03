db5a67c8ea3c8979d1bc0b7f359685d8777442f3
imortalmage1 <jbray2@gmail.com>
2020-11-02 20:41:35 -0600

    Add files via upload

--- main/java/com/red_warlock_/immortalis/util/RegistryHandler.java
+++ main/java/com/red_warlock_/immortalis/util/RegistryHandler.java
@@ -1,14 +1,15 @@
 package com.red_warlock_.immortalis.util;
 
 import com.red_warlock_.immortalis.ImmortalisMod;
+import com.red_warlock_.immortalis.armor.ModArmorMaterial;
 import com.red_warlock_.immortalis.blocks.BlockBase;
 import com.red_warlock_.immortalis.blocks.TestBlock;
 import com.red_warlock_.immortalis.items.BlockItemBase;
 import com.red_warlock_.immortalis.items.ItemBase;
 import com.red_warlock_.immortalis.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.SwordItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -35,6 +36,22 @@ public class RegistryHandler {
     //TOOLS
     public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
             new SwordItem(ModItemTier.RUBY, 5, -2.4f, new Item.Properties().group(ImmortalisMod.TAB)));
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () ->
+            new PickaxeItem(ModItemTier.RUBY, 5, -2.4f, new Item.Properties().group(ImmortalisMod.TAB)));
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () ->
+            new ShovelItem(ModItemTier.RUBY, 5, -2.4f, new Item.Properties().group(ImmortalisMod.TAB)));
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () ->
+            new AxeItem(ModItemTier.RUBY, 5, -2.4f, new Item.Properties().group(ImmortalisMod.TAB)));
+
+    //Armor
+    public static final RegistryObject<ArmorItem> RUBY_HELMET = ITEMS.register("ruby_helmet", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.HEAD, new Item.Properties().group(ImmortalisMod.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_CHESTPLATE = ITEMS.register("ruby_chestplate", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.CHEST, new Item.Properties().group(ImmortalisMod.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_LEGGINGS = ITEMS.register("ruby_leggings", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.LEGS, new Item.Properties().group(ImmortalisMod.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_BOOTS = ITEMS.register("ruby_boots", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.FEET, new Item.Properties().group(ImmortalisMod.TAB)));
 
 
 }
